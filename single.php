32b521fe1209820f0ceb4df837cead463d6c2e74
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-10-01 17:29:40 -0700

    Update files for online usage

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
       <link rel="shortcut icon" href="<?php echo esc_url( home_url( '/' ) ); ?>images/favicon.png">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
       <link rel="stylesheet" href="https://cdn.rawgit.com/Amine-Smahi/CDN/765d7683/styles-2.css" type="text/css">
-      <link href="http://fonts.googleapis.com/earlyaccess/droidarabickufi.css" rel="stylesheet">
+      <link rel="prefetch" type="text/css" onload="this.rel='stylesheet'" href="http://fonts.googleapis.com/earlyaccess/droidarabickufi.css">
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
    </head>
    <body class="material-template" data-js="open">
